ff5b526fe133555bcf6a6c8171a79c59dc7701c8
“XAXinAn” <jianghangximail@gmail.com>
2025-08-13 16:46:52 +0800

    创建任务的controller和service初步写好

--- ruoyi-admin/src/main/java/com/ruoyi/tps/controller/CreateTaskController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/tps/controller/CreateTaskController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.common.core.domain.AjaxResult;
 
 @RestController
 @RequestMapping("/tps/task")
