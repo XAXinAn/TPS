7cf09620125a1589ba025ac712dd7bdd5a36e67a
“XAXinAn” <jianghangximail@gmail.com>
2025-08-15 10:22:22 +0800

    将创建任务表单改为按钮弹出对话框

--- ruoyi-ui-Vue3/src/views/tps/create/index.vue
+++ ruoyi-ui-Vue3/src/views/tps/create/index.vue
@@ -1,53 +1,59 @@
 <template>
   <div class="app-container">
-    <el-form ref="formRef" :model="formData" :rules="rules" size="default" label-width="100px">
-      <el-form-item label="任务标题" prop="title">
-        <el-input v-model="formData.title" type="text" placeholder="请输入任务标题" clearable :style="{width: '100%'}"></el-input>
-      </el-form-item>
-      <el-form-item label="描述" prop="description">
-        <el-input v-model="formData.description" type="textarea" placeholder="请输入描述"
-                  :autosize="{minRows: 4, maxRows: 4}" :style="{width: '100%'}"></el-input>
-      </el-form-item>
-      <el-form-item label="截止日期" prop="deadline">
-        <el-date-picker v-model="formData.deadline" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
-                        :style="{width: '100%'}" placeholder="请选择截止日期" clearable></el-date-picker>
-      </el-form-item>
-      <el-form-item label-width="106px" label="下发对象类型" prop="recipientType">
-        <el-radio-group v-model="formData.recipientType" size="default">
-          <el-radio v-for="(item, index) in recipientTypeOptions" :key="index" :value="item.value"
-                    :disabled="item.disabled">{{item.label}}</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="下发对象" prop="orgId" v-if="formData.recipientType === 1">
-        <el-select v-model="formData.orgId" placeholder="请选择下发对象" multiple filterable clearable
-                   :style="{width: '100%'}" :loading="orgLoading">
-          <el-option v-for="item in orgOptions" :key="item.orgId" :label="item.org1Id + ' - ' + item.orgName"
-                     :value="item.orgId">
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="下发对象" prop="userId" v-if="formData.recipientType === 2">
-        <el-select v-model="formData.userId" placeholder="请选择下发对象" multiple filterable clearable
-                   :style="{width: '100%'}" :loading="userLoading">
-          <el-option v-for="item in userOptions" :key="item.userId" :label="item.userName + ' - ' + item.nickName"
-                     :value="item.userId">
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="优先级" prop="priority">
-        <el-input-number v-model="formData.priority" placeholder="优先级" controls-position=right></el-input-number>
-      </el-form-item>
-      <el-form-item label-width="106px" label="需发起人确认" prop="confirm" required>
-        <el-switch v-model="formData.confirm"></el-switch>
-      </el-form-item>
-      <el-form-item label="上传" prop="attachment">
-        <FileUpload v-model="formData.attachment" :file-size="50" :file-type="['doc', 'docx', 'png', 'jpg']"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm">提交</el-button>
-        <el-button @click="resetForm">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <el-button type="primary" @click="openDialog">创建任务</el-button>
+
+    <el-dialog v-model="dialogVisible" title="创建任务" width="600px" @close="resetForm">
+      <el-form ref="formRef" :model="formData" :rules="rules" size="default" label-width="100px">
+        <el-form-item label="任务标题" prop="title">
+          <el-input v-model="formData.title" type="text" placeholder="请输入任务标题" clearable :style="{width: '100%'}"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+          <el-input v-model="formData.description" type="textarea" placeholder="请输入描述"
+                    :autosize="{minRows: 4, maxRows: 4}" :style="{width: '100%'}"></el-input>
+        </el-form-item>
+        <el-form-item label="截止日期" prop="deadline">
+          <el-date-picker v-model="formData.deadline" format="YYYY-MM-DD" value-format="YYYY-MM-DD"
+                          :style="{width: '100%'}" placeholder="请选择截止日期" clearable></el-date-picker>
+        </el-form-item>
+        <el-form-item label-width="106px" label="下发对象类型" prop="recipientType">
+          <el-radio-group v-model="formData.recipientType" size="default">
+            <el-radio v-for="(item, index) in recipientTypeOptions" :key="index" :value="item.value"
+                      :disabled="item.disabled">{{item.label}}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="下发对象" prop="orgId" v-if="formData.recipientType === 1">
+          <el-select v-model="formData.orgId" placeholder="请选择下发对象" multiple filterable clearable
+                     :style="{width: '100%'}" :loading="orgLoading">
+            <el-option v-for="item in orgOptions" :key="item.orgId" :label="item.org1Id + ' - ' + item.orgName"
+                       :value="item.orgId">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="下发对象" prop="userId" v-if="formData.recipientType === 2">
+          <el-select v-model="formData.userId" placeholder="请选择下发对象" multiple filterable clearable
+                     :style="{width: '100%'}" :loading="userLoading">
+            <el-option v-for="item in userOptions" :key="item.userId" :label="item.userName + ' - ' + item.nickName"
+                       :value="item.userId">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="优先级" prop="priority">
+          <el-input-number v-model="formData.priority" placeholder="优先级" controls-position=right></el-input-number>
+        </el-form-item>
+        <el-form-item label-width="106px" label="需发起人确认" prop="confirm" required>
+          <el-switch v-model="formData.confirm"></el-switch>
+        </el-form-item>
+        <el-form-item label="上传" prop="attachment">
+          <FileUpload v-model="formData.attachment" :file-size="50" :file-type="['doc', 'docx', 'png', 'jpg']"/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">提交</el-button>
+          <el-button @click="closeDialog">取消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -62,6 +68,7 @@ const { proxy } = getCurrentInstance();
 const formRef = ref();
 
 // =========== 响应式状态定义 ===========
+const dialogVisible = ref(false);
 const userOptions = ref([]);
 const userLoading = ref(false);
 const orgOptions = ref([]);
@@ -95,13 +102,20 @@ const formState = reactive({
 });
 const { formData, rules } = toRefs(formState);
 
+// =========== Dialog控制 ===========
+function openDialog() {
+  dialogVisible.value = true;
+}
+
+function closeDialog() {
+  dialogVisible.value = false;
+}
 
 // =========== 生命周期钩子 ===========
 onMounted(() => {
   loadUsers();
   loadOrgs();
 });
-
 
 // =========== 数据加载方法 ===========
 const loadUsers = async () => {
@@ -136,7 +150,6 @@ const loadOrgs = async () => {
   }
 };
 
-
 // =========== 表单与事件处理方法 ===========
 function submitForm() {
   formRef.value.validate((valid) => {
@@ -155,6 +168,7 @@ function submitForm() {
     console.log('准备提交的表单数据:', submitData);
     createTask(submitData).then(res => {
       proxy.$modal.msgSuccess('提交成功');
+      closeDialog();
     }).catch(err => {
       proxy.$modal.msgError('提交失败');
     });
@@ -162,7 +176,9 @@ function submitForm() {
 }
 
 function resetForm() {
-  formRef.value.resetFields();
+  if (formRef.value) {
+    formRef.value.resetFields();
+  }
 }
 </script>
 
