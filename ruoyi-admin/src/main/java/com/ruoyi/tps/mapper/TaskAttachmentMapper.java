7fd97ff798e1d1d729e24907a98a1830dd7fa359
“XAXinAn” <jianghangximail@gmail.com>
2025-08-15 09:49:27 +0800

    增加代码生成任务附件菜单

--- ruoyi-admin/src/main/java/com/ruoyi/tps/mapper/TaskAttachmentMapper.java
+++ ruoyi-admin/src/main/java/com/ruoyi/tps/mapper/TaskAttachmentMapper.java
@@ -2,6 +2,7 @@ package com.ruoyi.tps.mapper;
 
 import java.util.List;
 import com.ruoyi.tps.domain.TaskAttachment;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 任务附件Mapper接口
@@ -9,6 +10,7 @@ import com.ruoyi.tps.domain.TaskAttachment;
  * @author ruoyi
  * @date 2025-08-15
  */
+@Mapper
 public interface TaskAttachmentMapper 
 {
     /**
